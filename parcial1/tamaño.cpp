420f9cda1632db01ebacf87bb5b3a386d2b3fbaa
Hector21s <124941160+Hector21s@users.noreply.github.com>
2023-02-14 22:23:04 +0000

    add hello.cpp test

---
+++ parcial1/tamaño.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace  std;
+
+
+int main() {
+// prints the string enclosed in double quotes 
+ cout << "Tamaño del caracter: " << sizeof(char) << " byte" << endl; 
+ cout << "Tamaño de int: " << sizeof(int) << "bytes" << endl;
+ cout << "Tamaño del float: " << sizeof(float) << "bytes" << endl;
+cout << "Tamaño del double: " << sizeof(double) << "bytes" << endl; 
+return 0;
+}
